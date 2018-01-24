9bb2a15d426aff8fe957ac60e71cab4846e36af4
jenchieh <jcs090218@gmail.com>
2018-01-24 14:11:28 +0800

    Add developer use installation executable.

---
+++ src/jcs_install_dev.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_install_dev.sh $
+# $Date: 2018-01-24 14:07:25 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+#
+# DESCRIPTION(jenchieh): Install for developer use.
+#
+
+
+echo "Start JayCeS dev installtion.."
+
+# URL(jenchieh): https://github.com/ggreer/the_silver_searcher
+apt-get install silversearcher-ag
+
+
+echo
+echo "Done JayCeS dev installtion.."
+echo

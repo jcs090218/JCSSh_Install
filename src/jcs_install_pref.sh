9b22a5e594b1bbf2beadd2e8c5d74ca400c5136d
jenchieh <jcs090218@gmail.com>
2018-01-24 14:13:30 +0800

    Rename jcs_preferences_install.sh to jcs_install_pref.sh for shorter and consistent naming.

---
+++ src/jcs_install_pref.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# ========================================================================
+# $File: jcs_install_pref.sh $
+# $Date: 2017-01-14 05:17:48 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright (c) 2017 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+# DESCRIPTION(jenchieh): this will install all the preference i
+# think that is useful for me.
+
+echo
+echo "-------- Start JayCeS preferences installation  ------------"
+echo
+
+
+# install tree
+apt-get install tree
+
+# install figlet
+apt-get install figlet
+
+# install compiz
+sudo apt-get install compizconfig-settings-manager
+
+
+echo
+echo "-------- End JayCeS preferences installation  ------------"
+echo
